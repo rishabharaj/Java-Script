d7733956300b5167baa0dc6d91705582420ae770
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-05-04 20:50:52 +0530

    Update concatenation.js

--- Java Script/concatenation.js
+++ Java Script/concatenation.js
@@ -5,3 +5,5 @@ longString += " a";
 longString += " time";
 longString += "...";
 console.log(longString); // "Once upon a time..."
+console.log(5 + "23") // 523 because 5 is converted to a string and concatenated with "23"
+console.log(5 + +"23"); // 28 because the unary plus operator converts "23" to a number and adds it to 5
