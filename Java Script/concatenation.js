85ebf8274a3c90a8ee7df5dd3cbedf9539782e77
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-05-04 21:10:12 +0530

    Update concatenation.js

--- Java Script/concatenation.js
+++ Java Script/concatenation.js
@@ -7,3 +7,8 @@ longString += "...";
 console.log(longString); // "Once upon a time..."
 console.log(5 + "23") // 523 because 5 is converted to a string and concatenated with "23"
 console.log(5 + +"23"); // 28 because the unary plus operator converts "23" to a number and adds it to 5
+// First plus for (Addition/Concatenation)
+// Second plus works as Unary Operator. 
+// Hope that makes it clear. 
+// But note that there has to be a space between those two plus signs.
+// If any confusion still persists please let me know.
